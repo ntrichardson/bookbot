f22024a47f6ce47f006688471810924a6f470063
ntrichardson <nkrichardson5@gmail.com>
2024-06-16 17:27:14 -0600

    Removed test prints from main.py file

--- main.py
+++ main.py
@@ -14,18 +14,13 @@ def characterCount(text):
 	return characters
 
 def report(char_count):
-	print(f"Dictionary: {char_count}")
 	charList = []
 	for key, value in char_count.items():
 		mini_dict = {"character": key, "num": value}  # Create mini dictionary
 		charList.append(mini_dict)  # Append mini dictionary to list
-	for char in charList:
-		print(char)
 	charList.sort(reverse=True, key = sort_on)
 	for char in charList:
 		print(f"The {char['character']} character was found {char['num']} times")
-
-#	print(charList)
 
 def sort_on(dict):
     return dict["num"]
